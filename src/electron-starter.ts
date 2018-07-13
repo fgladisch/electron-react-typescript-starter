3cc6aec887038a7d5979bacb9303982b421dd7a0
Felix Gladisch <felix@felixgladisch.de>
2018-07-13 11:08:28 +0200

    styles: formatting

--- src/electron-starter.ts
+++ src/electron-starter.ts
@@ -7,44 +7,43 @@ const { ELECTRON_DEV_URL } = process.env;
 const isDev = typeof ELECTRON_DEV_URL !== "undefined";
 
 const constructProdAssetPath = (assetPath: string, useFileProtocol: boolean) => {
-	const config: url.UrlObject = {
-		pathname: path.join(__dirname, "/../build/", assetPath),
-		slashes: true
-	};
-	if (useFileProtocol) {
-		config.protocol = "file:";
-	}
-	return url.format(config);
+  const config: url.UrlObject = {
+    pathname: path.join(__dirname, "/../build/", assetPath),
+    slashes: true
+  };
+  if (useFileProtocol) {
+    config.protocol = "file:";
+  }
+  return url.format(config);
 };
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow: BrowserWindow | null;
 
-function createWindow() {
-	// Create the browser window.
-	mainWindow = new BrowserWindow({ width: 1280, height: 720 });
+const createWindow = () => {
+  // Create the browser window
+  mainWindow = new BrowserWindow({ width: 1280, height: 720 });
 
-	// hide the menu bar
-	mainWindow.setMenuBarVisibility(false);
+  // hide the menu bar
+  mainWindow.setMenuBarVisibility(false);
 
-	// and load the index.html of the app.
-	const startUrl = ELECTRON_DEV_URL || constructProdAssetPath("index.html", true);
+  // and load the index.html of the app.
+  const startUrl = ELECTRON_DEV_URL || constructProdAssetPath("index.html", true);
+  mainWindow.loadURL(startUrl);
 
-	mainWindow.loadURL(startUrl);
+  if (isDev) {
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+  }
 
-	if (isDev) {
-		// Open the DevTools.
-		mainWindow.webContents.openDevTools();
-	}
-
-	// Emitted when the window is closed.
-	mainWindow.on("closed", () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		mainWindow = null;
-	});
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
 }
 
 // This method will be called when Electron has finished
@@ -54,19 +53,19 @@ app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow();
-	}
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
--- src/electron-wait-react.ts
+++ src/electron-wait-react.ts
@@ -8,23 +8,23 @@ const client = new Socket();
 
 let startedElectron = false;
 const tryConnection = () => {
-	// tslint:disable-next-line:no-console
-	console.log(`Trying to connect on port ${port}...`);
-	client.connect(
-		{ port },
-		() => {
-			client.end();
-			if (!startedElectron) {
-				startedElectron = true;
-				const exec = require("child_process").exec;
-				exec("npm run electron");
-			}
-		}
-	);
+  // tslint:disable-next-line:no-console
+  console.log(`Trying to connect on port ${port}...`);
+  client.connect(
+    { port },
+    () => {
+      client.end();
+      if (!startedElectron) {
+        startedElectron = true;
+        const exec = require("child_process").exec;
+        exec("npm run electron");
+      }
+    }
+  );
 };
 
 tryConnection();
 
 client.on("error", error => {
-	setTimeout(tryConnection, 1000);
+  setTimeout(tryConnection, 1000);
 });
